1f956c9d74705628e395a6ebd042787f387cba61
Umesh Yadav <umesh4257@gmail.com>
2018-05-25 15:35:06 +0530

    Fixed repetitive docker build.
    Signed-off-by: Umesh Yadav <umesh4257@gmail.com>

--- build.sh
+++ build.sh
@@ -1,18 +1,33 @@
 #!/usr/bin/env bash
 set -e
+
 # -printf "%h\n" or -exec direname {} ';'
+# find */ -type f -name "*Dockerfile" -exec direname {} ';' | sort -u
+
 # Exit on error in any of pipe commands
 set -o pipefail
 
+# Get changed dockerfiles after previous commit
+IFS=$'\n'
+changed_files=($(git diff HEAD~ --name-only -- "*Dockerfile"))
+unset IFS
+
+# Build and push docker images only if it is the master branch
 if [[ "$TRAVIS_BRANCH" == "master" ]]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-  for f in $(find */ -type f -name "*Dockerfile" -exec direname {} ';' | sort -u); do
+  for f in "${changed_files[@]}"; do
+    if ! [[ -e "$f" ]]; then
+      continue
+    fi
+
     build_dir=$(dirname $f)
     base=${build_dir%%\/*}
     tag=${build_dir##*\/}
+
     if [[ -z "$suite" ]]; then
       tag=latest
     fi
+
     docker build -t "{$DOCKER_USERNAME}/{$base}:{$tag}" "$build_dir"
     docker push "{$DOCKER_USERNAME}/{$base}:{$tag}"
     echo "[OK]: sucessfully pushed."
