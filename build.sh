3604f242361e07819f5e6e498fc7166acd13718e
Umesh Yadav <umesh4257@gmail.com>
2018-05-25 17:47:33 +0530

    Added heroku-cli Dockerfile and fixed docker image build.
    Signed-off-by: Umesh Yadav <umesh4257@gmail.com>

--- build.sh
+++ build.sh
@@ -9,11 +9,11 @@ set -o pipefail
 
 # Get changed dockerfiles after previous commit
 IFS=$'\n'
-changed_files="($(git diff HEAD~ --name-only -- "*Dockerfile"))"
+changed_files=( "$(git diff HEAD~ --name-only -- "*Dockerfile")" )
 unset IFS
 
 # Build and push docker images only if it is the master branch
-if [[ "$TRAVIS_BRANCH" == "master" && "${#changed_files[@]}" -eq 0 ]]; then
+if [[ "$TRAVIS_BRANCH" == "master" && "${#changed_files[@]}" -ne 0 ]]; then
   echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
   for f in "${changed_files[@]}"; do
     if ! [[ -e "$f" ]]; then
@@ -22,14 +22,14 @@ if [[ "$TRAVIS_BRANCH" == "master" && "${#changed_files[@]}" -eq 0 ]]; then
 
     build_dir=$(dirname "$f")
     base="${build_dir%%\/*}"
-    tag="${build_dir##*\/}"
-
+    tag="${build_dir##*}"
+    echo $base $tag
     if [[ -z "$tag" ]]; then
       tag=latest
     fi
 
-    docker build -t "{$DOCKER_USERNAME}/{$base}:{$tag}" "$build_dir"
-    docker push "{$DOCKER_USERNAME}/{$base}:{$tag}"
+    docker build -t "$DOCKER_USERNAME/$base:$tag" "$build_dir"
+    docker push "$DOCKER_USERNAME/$base:$tag"
     echo "[OK]: sucessfully pushed."
   done
 fi
